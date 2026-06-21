4c7e1a008dd625e36757489333fd53fea5d10291
eleconomistaincomodo <eleconomistaincomodo@outlook.com>
2026-06-21 12:37:33 -0600

    Create netlify-identity.js

---
+++ netlify-identity.js
@@ -0,0 +1,10 @@
+// Script de Netlify Identity para el panel de administración
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
